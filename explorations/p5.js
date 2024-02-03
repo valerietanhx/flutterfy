2af32b8d43e96216fbae81a6524d62207611829d
Valerie Tan <valerietanhx@gmail.com>
2024-02-04 00:41:14 +0800

    get multiple butterflies on canvas

--- explorations/p5.js
+++ explorations/p5.js
@@ -1,43 +1,57 @@
+let b1;
+let b2;
+
 function setup() {
   canvas = createCanvas(600, 600);
-  canvas.parent('viz');
+  canvas.parent("viz");
+  b1 = new Butterfly(width / 2, height / 2);
+  b2 = new Butterfly(400, 200);
 }
-
-let yoff = 0;
 
 function draw() {
   background(50);
-  translate(width/2, height/2);
-  stroke(255);
-  fill(240, 200, 220);
-  strokeWeight(0.7);
+  b1.display();
+  b2.display();
+}
 
-  let da = PI / 300;
-  let dx = 0.02;
-
-  beginShape();
-  let xoff = 0;
-
-  for (let a = -PI/2; a<=PI/2; a+=da) {
-    let n = noise(xoff, yoff);
-    n = 0.2;
-    let r = sin(2*a)*map(n, 0, 1, 50, 100);
-    let x = sin(yoff) * r*cos(a);
-    let y = r * sin(a);
-    xoff += dx;
-    vertex(x, y);
+class Butterfly {
+  constructor(cx, cy) {
+    this.cx = cx;
+    this.cy = cy;
+    this.yoff = 0;
   }
 
-  for (let a = PI/2; a<=3*PI/2; a+=da) {
-    let n = noise(xoff, yoff);
-    n = 0.8;
-    let r = sin(2*a)*map(n, 0, 1, 50, 100);
-    let x = sin(yoff) * r*cos(a);
-    let y = r * sin(a);
-    xoff -= dx;
-    vertex(x, y);
-  }
+  display() {
+    stroke(255);
+    fill(240, 200, 220); // to change to gradient
+    strokeWeight(0.7);
+    let da = PI / 300;
+    let dx = 0.02;
 
-  endShape();
-  yoff+= 0.02;
+    beginShape();
+    let xoff = 0;
+
+    for (let a = -PI / 2; a <= PI / 2; a += da) {
+      // let n = noise(xoff, this.yoff);
+      let n = 0.2;
+      let r = sin(2 * a) * map(n, 0, 1, 50, 100);
+      let x = sin(this.yoff) * r * cos(a);
+      let y = r * sin(a);
+      xoff += dx;
+      vertex(x + this.cx, y + this.cy);
+    }
+
+    for (let a = PI / 2; a <= (3 * PI) / 2; a += da) {
+      // let n = noise(xoff, this.yoff);
+      let n = 0.8;
+      let r = sin(2 * a) * map(n, 0, 1, 50, 100);
+      let x = sin(this.yoff) * r * cos(a);
+      let y = r * sin(a);
+      xoff -= dx;
+      vertex(x + this.cx, y + this.cy);
+    }
+
+    endShape();
+    this.yoff += 0.02;
+  }
 }
