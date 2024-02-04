917abca012a1c967985b3dd9c6fe3a25ff2ea7f3
Valerie Tan <valerietanhx@gmail.com>
2024-02-04 12:27:47 +0800

    figure out gradients for each object

--- explorations/p5.js
+++ explorations/p5.js
@@ -4,26 +4,45 @@ let b2;
 function setup() {
   canvas = createCanvas(600, 600);
   canvas.parent("viz");
-  b1 = new Butterfly(width / 2, height / 2);
-  b2 = new Butterfly(400, 200);
+  ctx = canvas.drawingContext;
+  b1 = new Butterfly(width / 2, height / 2, 0.02);
+  b2 = new Butterfly(400, 200, 0.01);
 }
 
 function draw() {
   background(50);
+
+  // move this into Butterfly class? or save details in setup() if not possible
+
+  push();
+  const grad = ctx.createRadialGradient(width / 2, height / 2, 0.1, width / 2, height / 2, 75);
+  grad.addColorStop(0, "black");
+  grad.addColorStop(1, "pink");
+  ctx.fillStyle = grad;
   b1.display();
+  pop();
+
+  const grad2 = ctx.createRadialGradient(400, 200, 0.1, 400, 200, 75);
+  grad2.addColorStop(0, "orange");
+  grad2.addColorStop(1, "yellow");
+  ctx.fillStyle = grad2;
   b2.display();
+  pop();
+
 }
 
 class Butterfly {
-  constructor(cx, cy) {
+  constructor(cx, cy, speed) {
     this.cx = cx;
     this.cy = cy;
+    this.speed = speed; // mode?
     this.yoff = 0;
   }
 
   display() {
+
     stroke(255);
-    fill(240, 200, 220); // to change to gradient
+    // fill(240, 200, 220); // to change to gradient
     strokeWeight(0.7);
     let da = PI / 300;
     let dx = 0.02;
@@ -52,6 +71,6 @@ class Butterfly {
     }
 
     endShape();
-    this.yoff += 0.02;
+    this.yoff += this.speed;
   }
 }
