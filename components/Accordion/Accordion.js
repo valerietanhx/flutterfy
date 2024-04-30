9b2f7be103ad96be52939089f342b773c06a6398
Valerie Tan <valerietanhx@gmail.com>
2024-04-30 21:55:28 +0800

    populate accordion items

--- components/Accordion/Accordion.js
+++ components/Accordion/Accordion.js
@@ -1,18 +1,81 @@
 import styles from "@/components/Accordion/accordion.module.css";
 import AccordionItem from "@/components/AccordionItem/AccordionItem";
+import { Instrument_Sans } from "next/font/google";
+
+const instrumentSans = Instrument_Sans({ weight: ["600"], subsets: ["latin"] });
 
 export default function Accordion() {
   return (
     <div className={styles.accordion}>
-      {/* to move to a separate data file*/}
-      <AccordionItem
-        summary="Lorem ipsum"
-        details="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tincidunt arcu non sodales neque sodales. Purus in mollis nunc sed. Cursus vitae congue mauris rhoncus aenean vel elit. Mattis ullamcorper velit sed ullamcorper morbi tincidunt. Faucibus turpis in eu mi bibendum neque egestas. Vitae et leo duis ut diam. Euismod lacinia at quis risus sed vulputate odio ut enim. Sed id semper risus in hendrerit gravida. Lorem ipsum dolor sit amet consectetur. Pulvinar neque laoreet suspendisse interdum consectetur. Cursus sit amet dictum sit amet justo donec enim. Quam pellentesque nec nam aliquam sem et tortor. Cras semper auctor neque vitae. Libero volutpat sed cras ornare arcu dui vivamus arcu felis. Condimentum id venenatis a condimentum. Tristique senectus et netus et malesuada fames. Lorem donec massa sapien faucibus. Elementum tempus egestas sed sed risus pretium quam. Tortor aliquam nulla facilisi cras."
-      />
-      <AccordionItem
-        summary="Nibh venenatis"
-        details="Nibh venenatis cras sed felis eget velit aliquet sagittis id. Nisi vitae suscipit tellus mauris a diam maecenas sed. Neque laoreet suspendisse interdum consectetur libero. Id porta nibh venenatis cras sed felis eget velit. Nunc mi ipsum faucibus vitae aliquet nec ullamcorper. Cursus eget nunc scelerisque viverra mauris in aliquam sem. Cursus metus aliquam eleifend mi in nulla posuere. Arcu odio ut sem nulla pharetra. Aliquet nibh praesent tristique magna sit amet purus. Fermentum et sollicitudin ac orci phasellus egestas tellus rutrum. Velit euismod in pellentesque massa placerat duis ultricies lacus sed. Libero id faucibus nisl tincidunt eget nullam non nisi. Facilisi nullam vehicula ipsum a. Odio ut sem nulla pharetra diam sit amet. Est lorem ipsum dolor sit amet consectetur. Velit dignissim sodales ut eu. Ac turpis egestas integer eget aliquet nibh praesent. In hendrerit gravida rutrum quisque non tellus orci. Ante metus dictum at tempor.        "
-      />
+      {/* would be neater to write in a separate data file,
+      e.g. import faq from "./faq.json" then map,
+      but not sure how to manage html elements, e.g. lists*/}
+
+      <AccordionItem title="How does the visualisation work?">
+        <p>
+          Each butterfly corresponds to a top track and seven of its audio
+          features provided by Spotify.
+        </p>
+        <ol>
+          <li>
+            The butterfly's{" "}
+            <span className={instrumentSans.className}>size</span> represents
+            the track's <span className={instrumentSans.className}>rank</span>.
+            The higher the track's rank, the bigger the butterfly.
+          </li>
+
+          <li>
+            The butterfly's{" "}
+            <span className={instrumentSans.className}>x-coordinate</span>{" "}
+            represents the track's{" "}
+            <span className={instrumentSans.className}>danceability</span>. The
+            higher the track's danceability, the greater the x-coordinate of the
+            butterfly.
+          </li>
+
+          <li>
+            The butterfly's{" "}
+            <span className={instrumentSans.className}>y-coordinate</span>{" "}
+            represents the track's{" "}
+            <span className={instrumentSans.className}>energy</span>. The higher
+            the track's energy, the greater the y-coordinate of the butterfly.
+          </li>
+
+          <li>
+            The butterfly's{" "}
+            <span className={instrumentSans.className}>top wing colour</span>{" "}
+            represents the track's{" "}
+            <span className={instrumentSans.className}>acousticness</span>.
+          </li>
+
+          <li>
+            The butterfly's{" "}
+            <span className={instrumentSans.className}>bottom wing colour</span>{" "}
+            represents the track's{" "}
+            <span className={instrumentSans.className}>valence</span>.
+          </li>
+
+          <li>
+            The butterfly's{" "}
+            <span className={instrumentSans.className}>
+              wing-flapping speed
+            </span>{" "}
+            represents the track's{" "}
+            <span className={instrumentSans.className}>tempo</span>. The faster
+            the track, the faster the butterfly.
+          </li>
+        </ol>
+      </AccordionItem>
+      <AccordionItem title="How was this website built?">
+        <p>
+          This website was built using <a href="https://nextjs.org/">Next.js</a>{" "}
+          and <a href="https://p5js.org/">p5.js</a>. It also relies on{" "}
+          <a href="https://developer.spotify.com/documentation/web-api">
+            Spotify's Web API
+          </a>{" "}
+          to get users' top tracks and their audio features.
+        </p>
+      </AccordionItem>
     </div>
   );
 }
--- components/AccordionItem/AccordionItem.js
+++ components/AccordionItem/AccordionItem.js
@@ -4,20 +4,18 @@ import styles from "@/components/AccordionItem/accordionItem.module.css";
 import { useState } from "react";
 
 export default function AccordionItem(props) {
-  const summary = props.summary;
-  const details = props.details;
-
+  const title = props.title;
   const [isExpanded, setIsExpanded] = useState(false);
 
   return (
     <div>
       <div
-        className={styles.summary}
+        className={styles.title}
         onClick={() => setIsExpanded(!isExpanded)}
       >
-        {summary}
+        {title}
       </div>
-      {isExpanded && <div className={styles.details}>{details}</div>}
+      {isExpanded && <div className={styles.details}>{props.children}</div>}
     </div>
   );
 }
