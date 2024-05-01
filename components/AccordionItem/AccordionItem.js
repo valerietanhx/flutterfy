aee55fa2eea53ba31d75052c4ba632b261b91eec
Valerie Tan <valerietanhx@gmail.com>
2024-05-01 22:12:08 +0800

    add accordion item transition

--- components/AccordionItem/AccordionItem.js
+++ components/AccordionItem/AccordionItem.js
@@ -1,21 +1,19 @@
-"use client"
+"use client";
 
 import styles from "@/components/AccordionItem/accordionItem.module.css";
 import { useState } from "react";
 
 export default function AccordionItem(props) {
-  const title = props.title;
   const [isExpanded, setIsExpanded] = useState(false);
 
   return (
     <div>
-      <div
-        className={styles.title}
-        onClick={() => setIsExpanded(!isExpanded)}
-      >
-        {title}
+      <div className={styles.title} onClick={() => setIsExpanded(!isExpanded)}>
+        {props.title}
       </div>
-      {isExpanded && <div className={styles.details}>{props.children}</div>}
+      <div className={`${styles.content} ${isExpanded && styles.show}`}>
+        {props.children}
+      </div>
     </div>
   );
 }
