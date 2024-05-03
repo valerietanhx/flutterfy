5c4ab62b348f828f75233049675bec06219a4cb0
Valerie Tan <valerietanhx@gmail.com>
2024-05-03 15:16:29 +0800

    style sketch + accordion

--- components/Sketch/constants.js
+++ components/Sketch/constants.js
@@ -2,35 +2,32 @@ export const SIZE_BASE_TOP = 12;
 export const SIZE_BASE_BOTTOM = (2 / 3) * SIZE_BASE_TOP;
 export const SHADOW_OFFSET = 2;
 
-// colour palette: https://loading.io/color/feature/Spectral-10/
 export const topGradientPalette = [
-  // potentially change palette so that all pairings of colours look better
   // dark
-  "#9e0142", // 0 < x <= 0.1
-  "#d53e4f", // 0.1 < x <= 0.2
-  "#f46d43", // 0.2 < x <= 0.3
-  "#fdae61", // 0.3 < x <= 0.4
-  "#fee08b", // 0.4 < x <= 0.5
-  "#e6f598", // 0.5 < x <= 0.6
-  "#abdda4", // 0.6 < x <= 0.7
-  "#66c2a5", // 0.7 < x <= 0.8
-  "#3288bd", // 0.8 < x <= 0.9
-  "#5e4fa2", // 0.9 < x <= 1
+  "#fb4434", // 0 < x <= 0.1
+  "#fa6492", // 0.1 < x <= 0.2
+  "#f77f00", // 0.2 < x <= 0.3
+  "#fa9f35", // 0.3 < x <= 0.4
+  "#fabf49", // 0.4 < x <= 0.5
+  "#479642", // 0.5 < x <= 0.6
+  "#469893", // 0.6 < x <= 0.7
+  "#559cf3", // 0.7 < x <= 0.8
+  "#7f87f7", // 0.8 < x <= 0.9
+  "#9c92fb", // 0.9 < x <= 1
 ];
 
-// colour palette: https://coolors.co/df015e-e27985-f8a58b-fed6ae-ffefc2-f2f9c8-d6eed2-99d6c3-5ea7d4-8376bc
 export const bottomGradientPalette = [
   // light
-  "#df015e",
-  "#e27985",
-  "#f8a58b",
-  "#fed6ae",
-  "#ffefc2",
-  "#f2f9c8",
-  "#d6eed2",
-  "#99d6c3",
-  "#5ea7d4",
-  "#8376bc",
+  "#fd9187",
+  "#ffb5cb",
+  "#ffba70",
+  "#facc75",
+  "#fcd588",
+  "#85c77f",
+  "#73bfba",
+  "#8dbcf7",
+  "#b3b7fa",
+  "#c8c3fd",
 ];
 
 // https://artlist.io/blog/music-bpm/
