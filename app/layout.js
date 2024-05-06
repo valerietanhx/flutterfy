c6298b87e7e91d4c05b3543795782e116208ddd9
Valerie Tan <valerietanhx@gmail.com>
2024-05-06 23:34:04 +0800

    update favicon
    closes #2

--- app/layout.js
+++ app/layout.js
@@ -13,6 +13,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link
+          rel="icon"
+          href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>🦋</text></svg>"
+        />
+      </head>
       <body className={instrumentSans.className}>{children}</body>
     </html>
   );
