9f8139c0d3e9283e45eeca419e5cc17d514bdc18
Valerie Tan <valerietanhx@gmail.com>
2024-05-07 21:59:20 +0800

    fix syntax errors
    closes #12

--- hooks/useSpotifyApi.js
+++ hooks/useSpotifyApi.js
@@ -5,33 +5,35 @@ export default function useSpotifyApi(url, options, key, dependencies) {
   const [data, setData] = useState(null);
 
   useEffect(() => {
-    fetch(url, options)
-      .then(async (response) => {
-        if (response.status == 401) {
-          const accessToken = await refreshAccessToken();
+    if (url) {
+      fetch(url, options)
+        .then(async (response) => {
+          if (response.status == 401) {
+            const accessToken = await refreshAccessToken();
 
-          const newOptions = {
-            headers: {
-              Authorization: `Bearer ${accessToken}`,
-            },
-          };
+            const newOptions = {
+              headers: {
+                Authorization: `Bearer ${accessToken}`,
+              },
+            };
 
-          return fetch(url, newOptions);
-        }
-        return response;
-      })
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error("An error occurred during fetching.");
-        }
-        return response;
-      })
-      .then((response) => {
-        return response.json();
-      })
-      .then((json) => {
-        setData(json[key]);
-      });
+            return fetch(url, newOptions);
+          }
+          return response;
+        })
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error("An error occurred during fetching.");
+          }
+          return response;
+        })
+        .then((response) => {
+          return response.json();
+        })
+        .then((json) => {
+          setData(json[key]);
+        });
+    }
   }, dependencies);
 
   return data;
